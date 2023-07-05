baf669c370ce6a521fe9bfa4e3f27ad93aeb8568
PavelSadykov <2439747@gmail.com>
2023-07-05 16:55:31 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Your event handling code here
+        }
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            // Your event handling code here
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            // Your event handling code here
+        }
+
+
+        private void InitializeComponent()
+        {
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonView = new System.Windows.Forms.Button();
+            this.textBoxStartDate = new System.Windows.Forms.TextBox();
+            this.textBoxEndDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.monthCalendar.Location = new System.Drawing.Point(301, 29);
+            this.monthCalendar.MaxSelectionCount = 1;
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 1;
+            this.monthCalendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar_DateChanged);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(188, 217);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(405, 20);
+            this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(188, 267);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(164, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonView
+            // 
+            this.buttonView.Location = new System.Drawing.Point(433, 267);
+            this.buttonView.Name = "buttonView";
+            this.buttonView.Size = new System.Drawing.Size(160, 23);
+            this.buttonView.TabIndex = 4;
+            this.buttonView.Text = "Просмотреть";
+            this.buttonView.UseVisualStyleBackColor = true;
+            this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
+            // 
+            // textBoxStartDate
+            // 
+            this.textBoxStartDate.Location = new System.Drawing.Point(493, 29);
+            this.textBoxStartDate.Name = "textBoxStartDate";
+            this.textBoxStartDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStartDate.TabIndex = 5;
+            // 
+            // textBoxEndDate
+            // 
+            this.textBoxEndDate.Location = new System.Drawing.Point(494, 83);
+            this.textBoxEndDate.Name = "textBoxEndDate";
+            this.textBoxEndDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEndDate.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Начальная дата";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(499, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Конечная дата";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(495, 154);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(98, 23);
+            this.buttonSearch.TabIndex = 10;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEndDate);
+            this.Controls.Add(this.textBoxStartDate);
+            this.Controls.Add(this.buttonView);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.monthCalendar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonView;
+        private TextBox textBoxStartDate;
+        private TextBox textBoxEndDate;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button buttonSearch;
+    }
+
+}     
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<DateTime, string> notes; // Dictionary to store notes for each date
+
+        public Form1()
+        {
+            InitializeComponent();
+            notes = new Dictionary<DateTime, string>();
+            monthCalendar.DateChanged += MonthCalendar_DateChanged; // Attach DateChanged event handler
+        }
+
+        private void MonthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTime selectedDate = monthCalendar.SelectionStart.Date;
+            if (notes.ContainsKey(selectedDate))
+            {
+                string note = notes[selectedDate];
+                textBox.Text = note;
+            }
+            else
+            {
+                textBox.Text = string.Empty;
+            }
+
+            // Update the bolded dates whenever the selected month changes
+            BoldDatesWithNotes();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = monthCalendar.SelectionStart.Date;
+            string note = textBox.Text.Trim();
+            if (!string.IsNullOrEmpty(note))
+            {
+                if (notes.ContainsKey(selectedDate))
+                {
+                    notes[selectedDate] = note;
+                }
+                else
+                {
+                    notes.Add(selectedDate, note);
+                }
+            }
+            textBox.Clear();
+
+            // Update the bolded dates after adding a new note
+            BoldDatesWithNotes();
+        }
+
+        private void buttonView_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = monthCalendar.SelectionStart.Date;
+            if (notes.ContainsKey(selectedDate))
+            {
+                string note = notes[selectedDate];
+                MessageBox.Show(note, "Note", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void BoldDatesWithNotes()
+        {
+            // Clear the bolded dates
+            monthCalendar.RemoveAllBoldedDates();
+
+            // Bold dates with notes
+            foreach (DateTime date in notes.Keys)
+            {
+                monthCalendar.AddBoldedDate(date);
+            }
+
+            monthCalendar.UpdateBoldedDates();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            DateTime startDate = DateTime.Parse(textBoxStartDate.Text);
+            DateTime endDate = DateTime.Parse(textBoxEndDate.Text);
+
+            var eventsInRange = notes.Where(pair => pair.Key >= startDate && pair.Key <= endDate);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var pair in eventsInRange)
+            {
+                sb.AppendLine($"{pair.Key.ToShortDateString()}: {pair.Value}");
+            }
+
+            MessageBox.Show(sb.ToString(), "Events within Range", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
