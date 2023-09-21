a441579f48e3fa0f872c13b5aa14520784a77c18
Antonio Renato Barbosa <alvoerrado2@gmail.com>
2023-09-21 11:47:57 -0300

    Update Form1.Designer.cs
    Translation of texts and labels from Russian into English

--- WindowsFormsApp9/Form1.Designer.cs
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -50,7 +50,8 @@ namespace WindowsFormsApp9
             // monthCalendar
             // 
             this.monthCalendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.monthCalendar.Location = new System.Drawing.Point(301, 29);
+            this.monthCalendar.Location = new System.Drawing.Point(278, 36);
+            this.monthCalendar.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.monthCalendar.MaxSelectionCount = 1;
             this.monthCalendar.Name = "monthCalendar";
             this.monthCalendar.TabIndex = 1;
@@ -58,88 +59,97 @@ namespace WindowsFormsApp9
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(188, 217);
+            this.textBox.Location = new System.Drawing.Point(251, 267);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(405, 20);
+            this.textBox.Size = new System.Drawing.Size(539, 22);
             this.textBox.TabIndex = 2;
             this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(188, 267);
+            this.buttonAdd.Location = new System.Drawing.Point(251, 329);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(164, 23);
+            this.buttonAdd.Size = new System.Drawing.Size(219, 28);
             this.buttonAdd.TabIndex = 3;
-            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.Text = "Add.";
             this.buttonAdd.UseVisualStyleBackColor = true;
             this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // buttonView
             // 
-            this.buttonView.Location = new System.Drawing.Point(433, 267);
+            this.buttonView.Location = new System.Drawing.Point(577, 329);
+            this.buttonView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonView.Name = "buttonView";
-            this.buttonView.Size = new System.Drawing.Size(160, 23);
+            this.buttonView.Size = new System.Drawing.Size(213, 28);
             this.buttonView.TabIndex = 4;
-            this.buttonView.Text = "Просмотреть";
+            this.buttonView.Text = "Browse";
             this.buttonView.UseVisualStyleBackColor = true;
             this.buttonView.Click += new System.EventHandler(this.buttonView_Click);
             // 
             // textBoxStartDate
             // 
-            this.textBoxStartDate.Location = new System.Drawing.Point(493, 29);
+            this.textBoxStartDate.Location = new System.Drawing.Point(686, 36);
+            this.textBoxStartDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxStartDate.Name = "textBoxStartDate";
-            this.textBoxStartDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStartDate.Size = new System.Drawing.Size(132, 22);
             this.textBoxStartDate.TabIndex = 5;
             // 
             // textBoxEndDate
             // 
-            this.textBoxEndDate.Location = new System.Drawing.Point(494, 83);
+            this.textBoxEndDate.Location = new System.Drawing.Point(686, 98);
+            this.textBoxEndDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxEndDate.Name = "textBoxEndDate";
-            this.textBoxEndDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEndDate.Size = new System.Drawing.Size(132, 22);
             this.textBoxEndDate.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(492, 52);
+            this.label1.Location = new System.Drawing.Point(686, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.Size = new System.Drawing.Size(64, 16);
             this.label1.TabIndex = 7;
-            this.label1.Text = "Начальная дата";
+            this.label1.Text = "Start date";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(373, 240);
+            this.label2.Location = new System.Drawing.Point(497, 295);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.Size = new System.Drawing.Size(33, 16);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Текст";
+            this.label2.Text = "Text";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(499, 117);
+            this.label3.Location = new System.Drawing.Point(683, 124);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.Size = new System.Drawing.Size(61, 16);
             this.label3.TabIndex = 9;
-            this.label3.Text = "Конечная дата";
+            this.label3.Text = "End date";
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(495, 154);
+            this.buttonSearch.Location = new System.Drawing.Point(686, 186);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSearch.Name = "buttonSearch";
-            this.buttonSearch.Size = new System.Drawing.Size(98, 23);
+            this.buttonSearch.Size = new System.Drawing.Size(131, 28);
             this.buttonSearch.TabIndex = 10;
-            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
             this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -150,6 +160,7 @@ namespace WindowsFormsApp9
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.monthCalendar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
